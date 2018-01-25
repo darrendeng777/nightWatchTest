d50fa880543422228589148e56f00cf36dcb22b3
darrendeng777 <darren.deng1@telus.com>
2018-01-25 14:30:40 -0500

    config file

--- build/selenium-start.js
+++ build/selenium-start.js
@@ -4,8 +4,7 @@ const seleniumConfig = require('./selenium-conf.js')
 selenium.start({
     drivers: seleniumConfig.driver
 }, function (err, child) {
-    if (err) throw new Error(`Selenium 启动失败: ${err}`)
-    console.log(`Selenium 已手工启动，进程 PID: ${child.pid}`)
-    console.log('当不再需要运行 Selenium 时可关闭此 PID 进程.')
-
-    //change something her
+    if (err) throw new Error(`Selenium start failed: ${err}`)
+    console.log(`Selenium has already started maually，process PID: ${child.pid}`)
+    console.log('when not need selenium we can close this process.')
+})
